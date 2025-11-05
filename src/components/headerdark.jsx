7ef7ff27db2ba3f43b4c55e3a5e84dc3ac410b85
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2025-11-05 18:28:14 +0530

    Update headerdark.jsx

--- src/components/headerdark.jsx
+++ src/components/headerdark.jsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import { useTheme } from "next-themes";
-import { Moon, Sun,  } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { ThemeToggler } from "@/components/animate-ui/primitives/effects/theme-toggler";
 
-export const ThemeTogglerBtn = ( ) => {
+export const ThemeTogglerBtn = () => {
   const { theme, resolvedTheme, setTheme } = useTheme();
 
   return (
@@ -13,15 +13,27 @@ export const ThemeTogglerBtn = ( ) => {
       theme={theme}
       resolvedTheme={resolvedTheme}
       setTheme={setTheme}
-      direction={'ltr'}
+      direction="ltr"
     >
       {({ effective, toggleTheme }) => {
-        const nextTheme =
-          effective === "dark" ? "light" :  "dark";
+        // If <html> has class="dark", force effective to 'dark'
+        const htmlIsDark =
+          typeof document !== "undefined" &&
+          document.documentElement.classList.contains("dark");
+
+        const effectiveFixed = htmlIsDark ? "dark" : effective;
+        const nextTheme = effectiveFixed === "dark" ? "light" : "dark";
 
         return (
-          <button onClick={() => toggleTheme(nextTheme)}  className="text-foreground hover:text-foreground !cursor-pointer hover:bg-sidebar-accent duration-300 p-2 rounded-md">
-            { effective === "dark" ? <Moon className="!h-5 !w-auto" /> : <Sun className="!h-5 !w-auto" />}
+          <button
+            onClick={() => toggleTheme(nextTheme)}
+            className="text-foreground hover:text-foreground !cursor-pointer hover:bg-sidebar-accent duration-300 p-2 rounded-md"
+          >
+            {effectiveFixed === "dark" ? (
+              <Moon className="!h-5 !w-auto" />
+            ) : (
+              <Sun className="!h-5 !w-auto" />
+            )}
           </button>
         );
       }}
