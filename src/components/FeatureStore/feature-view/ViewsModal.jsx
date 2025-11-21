6fc1ed8fd2f97b1207551dd3de3d152bcf449077
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2025-11-21 17:20:17 +0530

    update-[Ishita]
    changes

--- src/components/FeatureStore/feature-view/ViewsModal.jsx
+++ src/components/FeatureStore/feature-view/ViewsModal.jsx
@@ -17,25 +17,20 @@ export default function ViewsModal({ open, onOpenChange, onSelect }) {
       title: "Auto Create Features",
       description:
         "Quickly generate features by selecting tables. Our system will automatically create features from all columns.",
-      icon: (
-        <span className="text-xl text-[#E65A2E] font-bold select-none">𝛍</span>
-      ),
+      icon: <SynthWave className="w-6 h-6 text-primary" />,
     },
     {
       id: "manual",
       title: "Create Manually",
       description:
         "Full control over feature creation. Select specific columns, define joins, and apply transformations.",
-      icon: (
-        <span className="text-xl text-foreground font-bold select-none">𝛍</span>
-      ),
+      icon: <SynthWave className="w-6 h-6 text-primary" />,
     },
   ];
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="w-[80%] h-[80%] left-1/2  -translate-x-1/2 top-1/2 flex flex-col p-6">
-
+      <DialogContent className="w-[80%] h-[80%] left-1/2  -translate-x-1/2 top-1/2 flex flex-col p-6 px-7 ">
         <AnimatePresence mode="wait">
           {open && (
             <motion.div
@@ -52,20 +47,20 @@ export default function ViewsModal({ open, onOpenChange, onSelect }) {
                 </DialogTitle>
               </DialogHeader>
 
-              <div className="w-full grid grid-cols-2 gap-6  py-6 h-[70%]">
+              <div className="w-full grid grid-cols-2 gap-4  mt-14 h-[55%]">
                 {options.map((item) => (
                   <div
                     key={item.id}
                     onClick={() => onSelect?.(item.id)}
                     className={cn(
-                      "cursor-pointer p-8 rounded-lg border border-border-color-2 bg-gray-100",
-                      " transition-shadow duration-300",
-                      "flex flex-col items-center text-center space-y-4"
+                      "cursor-pointer p-8 group rounded-lg border  border-border-color-2 hover:bg-sidebar-accent duration-500 transition-all  flex items-center justify-center flex-col text-center space-y-4"
                     )}
                   >
-                    {item.icon}
-                    <h3 className="text-lg font-semibold">{item.title}</h3>
-                    <p className="text-sm text-foreground/80 max-w-[80%]  ">
+                    <div className="pb-2 group-hover:text-primary duration-500 transition-all">
+                      {item.icon}
+                    </div>
+                    <h3 className="text-2xl font-medium">{item.title}</h3>
+                    <p className="text-xs text-foreground/80 max-w-[80%]  ">
                       {item.description}
                     </p>
                   </div>
@@ -74,7 +69,6 @@ export default function ViewsModal({ open, onOpenChange, onSelect }) {
             </motion.div>
           )}
         </AnimatePresence>
-
       </DialogContent>
     </Dialog>
   );
